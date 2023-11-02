ea112cb8425975b1667129a0f373001809426584
natob <natob@REBARROS>
2023-11-02 17:14:23 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Pong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.playgeound = new System.Windows.Forms.Panel();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.racket = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.playgeound.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.racket)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // playgeound
+            // 
+            this.playgeound.Controls.Add(this.ball);
+            this.playgeound.Controls.Add(this.racket);
+            this.playgeound.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.playgeound.Location = new System.Drawing.Point(0, 0);
+            this.playgeound.Name = "playgeound";
+            this.playgeound.Size = new System.Drawing.Size(800, 450);
+            this.playgeound.TabIndex = 1;
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Red;
+            this.ball.Location = new System.Drawing.Point(382, 174);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(30, 30);
+            this.ball.TabIndex = 3;
+            this.ball.TabStop = false;
+            // 
+            // racket
+            // 
+            this.racket.BackColor = System.Drawing.Color.Black;
+            this.racket.Location = new System.Drawing.Point(348, 374);
+            this.racket.Name = "racket";
+            this.racket.Size = new System.Drawing.Size(200, 20);
+            this.racket.TabIndex = 2;
+            this.racket.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.playgeound);
+            this.Name = "Form1";
+            this.Text = "playground";            
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.playgeound.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.racket)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel playgeound;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox racket;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pong
+{
+    public partial class Form1 : Form
+    {
+        public int speed_left = 4; //velocidade do projetil
+        public int speed_top = 4;
+        public int point = 0;  //Pontos 
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer1.Enabled = true;
+            Cursor.Hide(); // Hide the cursor
+
+            this.FormBorderStyle = FormBorderStyle.None;// remover qualquer borda
+            this.TopMost = true;                     //Bring
+            this.Bounds = Screen.PrimaryScreen.Bounds; //Make in fulscrren
+
+            racket.Top = playgeound.Bottom - (playgeound.Bottom / 10); // indica posição da quete
+
+
+
+
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        { 
+            racket.Left = Cursor.Position.X - (racket.Width / 2); //Marca o centro da racket a posição do cursor
+
+            ball.Left += speed_left; //Mover projetil
+            ball.Top += speed_top;
+
+            if (ball.Bottom >= racket.Top && ball.Bottom <= racket.Bottom && ball.Left >= racket.Left && ball.Right <= racket.Right) //Colisção da racket
+
+            {
+                speed_top += 2; 
+                speed_left += 2;
+                speed_top = -speed_top; //muda a direção
+                point += 1;
+                speed_left = (speed_left > 0) ? speed_left + 2 : speed_left - 2;
+
+            }
+            
+            if (ball.Left <= playgeound.Left)
+            {
+                speed_left = -speed_left;
+
+            }
+            if (ball.Right >= playgeound.Right)
+            {
+                speed_left = -speed_left;
+            }
+
+            if (ball.Top <= playgeound.Top)
+            {
+                speed_top = -speed_top;
+            }
+
+            if (ball.Bottom >= playgeound.Bottom)
+            {
+                timer1.Enabled = false;   //Bola fora -> para game
+
+            }
+        }
+
+     
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                { this.Close(); } //Press Escape to Quit
+            }
+        }
+
+      
+    }
+}
